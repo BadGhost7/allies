ae8183f5488292dc330d2436f6e7aa31ff20c0a1
BadGhost7 <skrim2005@mail.ru>
2025-06-11 20:00:33 +0300

    ну хз регистрация работает а логин нет надо в пятницу разбираться

--- users/views.py
+++ users/views.py
@@ -4,7 +4,16 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'  
+
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/registration/signup.html'  # Custom path
+    
+
+   
